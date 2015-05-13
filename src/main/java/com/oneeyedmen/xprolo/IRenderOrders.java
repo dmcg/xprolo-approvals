912a9f06bf7e91d9724efbbfe70d223c30599ffd
Duncan McGregor <duncan@oneeyedmen.com>
2015-05-13 08:12:14 +0100

    More fix in light of rehearsal

--- src/main/java/com/oneeyedmen/xprolo/IRenderOrders.java
+++ src/main/java/com/oneeyedmen/xprolo/IRenderOrders.java
@@ -1,11 +1,12 @@
 package com.oneeyedmen.xprolo;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import static com.oneeyedmen.xprolo.Domain.Order;
 
 public interface IRenderOrders {
 
-    public String render(Order order) throws IOException;
+    public void render(OutputStream os, Order order) throws IOException;
 
 }

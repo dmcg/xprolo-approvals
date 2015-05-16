61fd66bdb47e41b8c5590cacb0f4174974910cb6
Duncan McGregor <duncan@oneeyedmen.com>
2015-05-16 12:49:50 +0100

    Improve the intro

--- src/test/java/com/oneeyedmen/xprolo/JsonOrderRendererTest.java
+++ src/test/java/com/oneeyedmen/xprolo/JsonOrderRendererTest.java
@@ -2,7 +2,12 @@ package com.oneeyedmen.xprolo;
 
 public class JsonOrderRendererTest {
 
-    // You're probably already using Approvals Tests
-    // We'll look at how they can lead to easy test wins
+    // You're probably already using Approvals Tests.
+    // We'll look at how they can improve your testing.
+
+    // Our mission is to implement JsonOrderRenderer -
+    // "Just write it to JSON, the mobile team will read whatever format you specify within reason"
+
+    // But maybe it would be good to know that we don't change it once the app is in the App Store.
 
 }
